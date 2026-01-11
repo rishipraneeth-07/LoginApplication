bc1aa95dcecb280578a45402155e8111a0334336
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-01-11 10:32:25 +0530

    Initial commit

---
+++ src/test/java/com/todo/loginapplication/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.loginapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
