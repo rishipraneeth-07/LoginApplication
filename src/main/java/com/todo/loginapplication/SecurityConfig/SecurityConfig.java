6765a7690cfa12a565d96341de2faeaf005b9260
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-01-12 13:22:38 +0530

    Added Userdetailservice(for testing)

--- src/main/java/com/todo/loginapplication/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/todo/loginapplication/SecurityConfig/SecurityConfig.java
@@ -3,15 +3,18 @@ package com.todo.loginapplication.SecurityConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/login").permitAll()
@@ -28,4 +31,14 @@ public class SecurityConfig {
             );
     return http.build();
     }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
 }
