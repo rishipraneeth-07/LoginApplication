c0a041fbda946f5c24c85e7c0f2d4d118781ef5c
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-01-12 13:27:52 +0530

    Login Controller

---
+++ src/main/java/com/todo/loginapplication/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.todo.loginapplication.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
