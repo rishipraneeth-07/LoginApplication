f5eaced25e39c12b4d14f5b8939d8bf42232b95b
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-01-13 11:19:46 +0530

    configured security and login page

--- src/main/java/com/todo/loginapplication/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/todo/loginapplication/SecurityConfig/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/login").permitAll()
+                    .requestMatchers("/login","/css/**").permitAll()
                     .anyRequest().authenticated()
             )
             .formLogin(form -> form
